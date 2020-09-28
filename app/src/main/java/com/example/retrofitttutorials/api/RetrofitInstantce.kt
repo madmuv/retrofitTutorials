d643428f2ce0be3345ea271cc58bf43956eba08c
Donyawan K <donyawa.kri@bigc.co.th>
2020-09-28 10:26:37 +0700

    add repository 5:18

---
+++ app/src/main/java/com/example/retrofitttutorials/api/RetrofitInstantce.kt
@@ -0,0 +1,18 @@
+package com.example.retrofitttutorials.api
+
+import com.example.retrofitttutorials.util.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstantce {
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val api:SimpleApi by lazy {
+        retrofit.create(SimpleApi::class.java)
+    }
+}
